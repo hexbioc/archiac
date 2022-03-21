64e1c5a04240e8ca1299d84d1c755665fd4dea0c
hexbioc <vaibhav.nayak@gmail.com>
2022-03-22 03:50:43 +0530

    Add .zshrc and neofetch to configuration

--- scripts/postboot/shell.sh
+++ scripts/postboot/shell.sh
@@ -2,9 +2,11 @@
 
 source "$(dirname $0)/common.sh"
 
-sudo pacman --noconfirm -S zsh xterm alacritty
-ln -s "$REPO_DIR/configurations/alacritty" ~/.config/alacritty
+sudo pacman --noconfirm -S zsh xterm alacritty neofetch
+ln --force -s "$REPO_DIR/configurations/alacritty" ~/.config/alacritty
 
-# Setup zsh
+# Setup zsh and neofetch
 sudo usermod --shell $(which zsh) $USER
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ln --force -s "$REPO_DIR/dotfiles/.zshrc" ~/.zshrc
+ln --force -s "$REPO_DIR/configurations/neofetch" ~/.config/neofetch
