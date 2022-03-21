251b7bb9b758a968c6156a7fbbfb245751cc53e1
hexbioc <vaibhav.nayak@gmail.com>
2022-03-22 04:07:49 +0530

    Add docker, docker compose and jq installation

--- scripts/postboot/developer.sh
+++ scripts/postboot/developer.sh
@@ -4,9 +4,25 @@ source "$(dirname $0)/common.sh"
 
 sudo pacman --noconfirm -S \
     base-devel \
+    jq \
+    docker \
     firefox-developer-edition
 
+# Setup docker
+sudo systemctl enable docker.service
+sudo usermod -aG docker $USER
+sudo systemctl start docker
+
+# Setup docker compose v2
+validate_credentials
+sudo mkdir -p /usr/local/lib/docker/cli-plugins
+sudo curl -SL \
+    https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 \
+    -o /usr/local/lib/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+
 # Install yay
+validate_credentials
 cd /opt/
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER yay-git/
