b38315e2389bf43851ab432c0741278b11641aa2
hexbioc <vaibhav.nayak@gmail.com>
2022-03-29 11:55:26 +0530

    Begin qtile config modularization

---
+++ configurations/qtile/custom/screens.py
@@ -0,0 +1,74 @@
+import subprocess
+
+from libqtile import bar, widget
+from libqtile.config import Screen
+from libqtile.log_utils import logger
+
+
+def _get_monitor_count():
+    xrandr_cmd = "xrandr --listactivemonitors | sed -n -r 's/^Monitors: ([0-9]+)/\1/p'"
+    try:
+        with subprocess.Popen(xrandr_cmd, stdout=subprocess.PIPE, shell=True) as proc:
+            return int(proc.stdout.read())
+    except Exception:
+        logger.exception("Error while trying to count monitors")
+
+    return 1
+
+def _get_secondary_bar():
+    return bar.Bar(
+        [
+            widget.CurrentLayout(),
+            widget.GroupBox(),
+            widget.Prompt(),
+            widget.WindowName(),
+            widget.Chord(
+                chords_colors={
+                    "launch": ("#ff0000", "#ffffff"),
+                },
+                name_transform=lambda name: name.upper(),
+            ),
+            widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+        ],
+        36,
+        # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+        # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+    )
+
+def _get_secondary_screens():
+    return [
+        Screen(
+            bottom=_get_secondary_bar(),
+        ) for _ in range(_get_monitor_count())
+    ]
+
+
+_primary_bar = bar.Bar(
+    [
+        widget.CurrentLayout(),
+        widget.GroupBox(),
+        widget.Prompt(),
+        widget.WindowName(),
+        widget.Chord(
+            chords_colors={
+                "launch": ("#ff0000", "#ffffff"),
+            },
+            name_transform=lambda name: name.upper(),
+        ),
+        widget.Systray(),
+        widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+        widget.QuickExit(),
+    ],
+    36,
+    # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+    # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+)
+
+_primary_screen = Screen(
+    bottom=_primary_bar,
+)
+
+screens = [
+    _primary_screen,
+    *_get_secondary_screens(),
+]
