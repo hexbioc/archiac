3bc34062a8775b1aa969da7299bbfc9447a33d08
hexbioc <vaibhav.nayak@gmail.com>
2022-04-03 09:01:12 +0530

    Add gtk theme config

--- configurations/qtile/config.py
+++ configurations/qtile/config.py
@@ -142,6 +142,7 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="lxappearance"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
     ]
